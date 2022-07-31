724fc6cf50f03e0564ffe4e12402f9fb94d10a94
Marcel Konieczny <marcel.konieczny@gmail.com>
2022-07-31 21:34:10 +0200

    Basic simulation

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/marcel/inz_ws/src
+# Build directory: /home/marcel/inz_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simulation")
